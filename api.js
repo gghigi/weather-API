666104ec74aaaec684b0b3e7d6c758ffc7e3e01d
gghigi <23176223+gghigi@users.noreply.github.com>
2021-02-24 16:05:27 +1100

    Added all JS

--- api.js
+++
@@ -1,2 +0,0 @@
-const weatherAPI = "03e95026372d7898965c970489aecc33";
-
--- index.js
+++ index.js
@@ -3,26 +3,51 @@ const goButton = document.querySelector('#button');
 const inputField = document.querySelector('#input-field');
 const card = document.querySelector('#card-results');
 
-
-
-goButton.addEventListener('click', () => {
-    console.log('hi')
-    //const cardHtml = createCardHtml(input.value);
-    //weatherContainer.innerHTML = cardHtml;
-    
-});
-
-const createCardHtml = (id, appid) => `
+const createCardHtml = (tempMax, tempMin, weather, wind) => `
 <div class="card" id="card-results">
-            <div class="card-body">
-                ${cityWeather}
+            <div class="card-body1">
+                ${tempMax}
+            </div>
+            <div class="card-body2">
+                ${tempMin}
+            </div>
+            <div class="card-body3">
+                ${weather}
+            </div>
+            <div class="card-body4">
+                ${wind}
             </div>
         </div>
         `;
 
 
-const cityWeather = input.value;
+goButton.addEventListener('click', () => {
+    
 
-fetch('api.openweathermap.org/data/2.5/weather?id={city id}&appid={API key}')
+
+
+
+const cityName = "sydney";
+const keyAPI = "03e95026372d7898965c970489aecc33";
+//const card = input.value;
+
+fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${keyAPI}&units=metric`)
   .then(response => response.json())
-  .then(data => console.log(data));
+  .then(data => {console.log();
+
+    
+    
+    const tempMax = data.main.temp_max;
+    const tempMin = data.main.temp_min;
+    const weather = data.weather[0].main;
+    const wind = data.wind.speed;
+
+
+    const cardHtml = createCardHtml(tempMax, tempMin, weather, wind);
+    card.innerHTML = cardHtml;})
+
+/*main.temp_min,
+  main.temp_max,
+  weather[0].main, 
+  wind.speed*/
+});
